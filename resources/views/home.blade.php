57ee1461bd31bf7dce3a1b7d2e8de7c9819ae9ca
badhonx3 <15203040@iubat.edu>
2019-07-13 00:20:12 +0600

    NB | 2nd card added for product

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,23 @@
                     <a href="#" class="btn btn-primary stretched-link">Buy</a>
 
                 </div>
+            </div>
+
+            <div class="card" style="width:200px; float: left; margin: 10px" >
+                    <img class="card-img-top" src="/assests/ad1.jpg" alt="Card image" style="width:50%">
+                    <div class="card-body">
+                        <h4 class="card-title">John Doe</h4>
+                        <p class="card-text">Some example text some example text. John Doe is an architect
+                            and engineer</p>
+
+                        <a href="#" class="btn btn-primary stretched-link">Add to cart</a>
+
+
+
+                        <a href="#" class="btn btn-primary stretched-link">Buy</a>
+
+                    </div>
+                </div>
             </a>
 
 
